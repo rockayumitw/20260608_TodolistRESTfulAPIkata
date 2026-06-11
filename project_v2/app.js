bb97ef2f732eba35e068348fd6b7ee496bc3fedc
rockayumitw <rockayumitw@gmail.com>
2026-06-11 14:09:02 +0000

    fix: 格式修正

--- project_v2/app.js
+++ project_v2/app.js
@@ -6,6 +6,6 @@ const server = http.createServer(requestListener);
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, '0,0,0,0',() => {
+server.listen(PORT, '0.0.0.0',() => {
   console.log(`server run, on: ${PORT}`);
 });
