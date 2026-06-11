07747ed0c4f151faa7f8f0a1c5ebba701bca9612
rockayumitw <rockayumitw@gmail.com>
2026-06-11 14:06:55 +0000

    fix: port

--- project_v2/app.js
+++ project_v2/app.js
@@ -4,8 +4,8 @@ const http = require("http");
 const requestListener = require("./server");
 const server = http.createServer(requestListener);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
+server.listen(PORT, '0,0,0,0',() => {
   console.log(`server run, on: ${PORT}`);
 });
